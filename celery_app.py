a5436848ab1f494b77cd3aa8f9e7f55685024812
KaiYe-ttt <yechenkai.2026@qq.com>
2026-04-02 22:13:29 +0800

    Update celery_app.py

--- celery_app.py
+++ celery_app.py
@@ -3,7 +3,7 @@ from config import REDIS_URL
 
 # 创建Celery实例
 celery_app = Celery(
-    "worker",        # worker名称
-    broker=REDIS_URL,  # 消息队列（任务发送）
-    backend=REDIS_URL  # 结果存储
+    "worker",       
+    broker=REDIS_URL, 
+    backend=REDIS_URL  
 )
