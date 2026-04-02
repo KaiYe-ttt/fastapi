89e13d7b99d95a4ba0f277e5cea67404b9fdf797
KaiYe-ttt <yechenkai.2026@qq.com>
2026-04-02 22:16:24 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,8 @@
 from sqlalchemy import Column, Integer, String
 from db import Base
 
-# 用户表模型（对应数据库users表）
 class User(Base):
-    __tablename__ = "users"  # 表名
-
-    id = Column(Integer, primary_key=True, index=True)  # 主键
-    username = Column(String, unique=True, index=True)  # 用户名（唯一）
-    password = Column(String)  # 密码（存加密后的）
+    __tablename__ = "users"  
+    id = Column(Integer, primary_key=True, index=True)  
+    username = Column(String, unique=True, index=True)  
+    password = Column(String)  
